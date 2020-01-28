cd7006e17381f058663513121277220ec5911844
Umberto <umbertopumpo@gmail.com>
2020-01-28 19:02:20 +0100

    Aggiunta parte gestione indici

--- Program.cs
+++ Program.cs
@@ -10,89 +10,87 @@ namespace PizzaPizza
     {
         public static int i { get; private set; }
         public static int j { get; private set; }
-        public static int numeroGiri { get; private set; }
-        public static int ciclo { get; private set; }
-        public static int valore { get; private set; }
-        public static int maxValore { get; private set; }
-        public static string stringaIndici { get; private set; }
+        public static int cicle { get; private set; }
+        public static string stringOfIndex { get; private set; }
+        public static string[] listOfIndex { get; private set; }
+        public static int value { get; private set; }
+        public static int maxValue { get; private set; }
 
         static void Main(string[] args)
         {
-            int somma = 0;
-            int obiettivo = 100;
-            int[] arrayInt = new int[10];
-            arrayInt[0] = 4;
-            arrayInt[1] = 14;
-            arrayInt[2] = 15;
-            arrayInt[3] = 18;
-            arrayInt[4] = 29;
-            arrayInt[5] = 32;
-            arrayInt[6] = 36;
-            arrayInt[7] = 82;
-            arrayInt[8] = 95;
-            arrayInt[9] = 95;
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\umber\Source\Repos\umby89\PizzaPizza\input\c_medium.in");
+
+            string[] splitLine1 = lines[0].Split(' ');
+            string[] splitLine2 = lines[1].Split(' ');
+            int maxSlice = Convert.ToInt32(splitLine1[0]);
+            int typeOfPizza = Convert.ToInt32(splitLine1[1]);
+            int[] pizzaValue =  splitLine2.Select(int.Parse).ToArray();
+            int sum = 0;                   
             List<string> bestValue = new List<string>();
 
-            for ( i = 0; i < arrayInt.Length; i++)
+            for ( i = 0; i < pizzaValue.Length; i++)
             {
-                somma = arrayInt[i];
+                sum = pizzaValue[i];
                 bestValue.Add(i.ToString()+"-Indice");
-                if (i + 1 < arrayInt.Length)
+                if (i + 1 < pizzaValue.Length)
                 {
-                    ciclo = i + 1;
-                    for ( j = i + 1; j < arrayInt.Length; j++)
+                    cicle = i + 1;
+                    for ( j = i + 1; j < pizzaValue.Length; j++)
                     {
                        
-                        somma = somma + arrayInt[j];
-                        if (somma <= obiettivo)
+                        sum = sum + pizzaValue[j];
+                        if (sum <= maxSlice)
                         {
                             bestValue.Add(j.ToString() + "-Indice");
                         }
                         else
                         {
-                            bestValue.Add((somma - arrayInt[j]).ToString());
-                            if (somma - arrayInt[j] == obiettivo)
+                            bestValue.Add((sum - pizzaValue[j]).ToString());
+                            if (sum - pizzaValue[j] == maxSlice)
                             {
-                                trovaIlMassimo(bestValue);
+                                findMax(bestValue);
                                 break;
                             }
-                            j = ciclo++;
-                            somma = arrayInt[i];
+                            j = cicle++;
+                            sum = pizzaValue[i];
                             bestValue.Add(i.ToString() + "-Indice");
                         }
                     }
                 }      
                 else
                 {
-                    j = arrayInt.Length;
+                    j = pizzaValue.Length;
                 }
-                if(i+1 == arrayInt.Length)
+                if(i+1 == pizzaValue.Length)
                 {
-                    trovaIlMassimo(bestValue);
+                    findMax(bestValue);
                 }
             }
         }
-        private static void trovaIlMassimo(List<string> bestValue)
+
+        //metodo per estrarre il massimo valore e indici allegati dalla lista
+        private static void findMax(List<string> bestValue)
         {
             for (int z = 0; z < bestValue.Count; z++)
             {
                 if (!bestValue[z].Contains("Indice"))
                 {
-                    valore = Convert.ToInt32(bestValue[z]);
-                    if (valore > maxValore)
-                        maxValore = valore;
+                    value = Convert.ToInt32(bestValue[z]);
+                    if (value > maxValue)
+                        maxValue = value;
                 }
             }
 
             for (int x = bestValue.Count-1; x > 0; x--)
             {
-                if (bestValue[x].Contains(maxValore.ToString()))
+                if (bestValue[x].Contains(maxValue.ToString()))
                 {
                     for (int y = x; y > 0; y--)
                     {
                         if (bestValue[y-1].Contains("Indice"))
                         {
-                            stringaIndici = stringaIndici + "-" + bestValue[y-1];
+                            stringOfIndex = stringOfIndex + "-" + bestValue[y-1];
                         }
                         else
                         {
@@ -100,6 +98,8 @@ namespace PizzaPizza
                         }
                     }
                 }
+                stringOfIndex = stringOfIndex.Replace("Indice", "/").Replace("-", "");
+                listOfIndex = stringOfIndex.Split('/'); // INDICI per ottenere il massimo risultato
                 //Gestione stringe indici per trovare i valori che compongono il valore max
             }
         }
